2d5f7df15c6ec2130b6dae2abc624a63f6c6ca47
Solving-Enrico <cristofanillienrico@gmail.com>
2021-06-11 10:58:09 +0200

    alessandro mi ha detto di fare commit

---
+++ src/test/java/it/prova/notify/NotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.notify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
